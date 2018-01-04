cf2526d9bf600c42a768b8b44c5f3c608bab729a
zwm <mingforregister@163.com>
2018-01-04 23:56:37 +0800

    First compile pass!

---
+++ keil_proj/source/mcu.h
@@ -0,0 +1,28 @@
+#ifndef	__MCU__
+#define	__MCU__
+
+#include "stc15fxxxx.h"
+#include "proj_lib.h"
+
+//---------------------------------------------------------------------------
+// CLOCK: 6MHz
+//---------------------------------------------------------------------------
+#define MAIN_Fosc		 6000000L	//定义主时钟
+//#define MAIN_Fosc		12000000L	//定义主时钟
+//#define MAIN_Fosc		11059200L	//定义主时钟
+//#define MAIN_Fosc		 5529600L	//定义主时钟
+//#define MAIN_Fosc		24000000L	//定义主时钟
+#define Main_Fosc_KHZ	(MAIN_Fosc / 1000)
+
+// type define
+typedef unsigned short int uint16;
+typedef unsigned char uchar;
+
+//---------------------------------------------------------------------------
+// Function Declearation
+//---------------------------------------------------------------------------
+void mcu_init (void);
+void mcu_set_exint (unsigned char idx, unsigned char mode);
+void mcu_set_tmr (unsigned char idx, unsigned char mode);
+
+#endif
---
+++ keil_proj/source/proj_lib.h
@@ -0,0 +1,87 @@
+#ifndef __PROJ_LIB__
+#define __PROJ_LIB__
+
+#include "mcu.h"
+#include "delay.h"
+#include "lcd1602.h"
+
+// FSM
+// 0 : FSM_TUS0
+// 1 : FSM_TUS1
+// 2 : FSM_WAIT
+#define FSM_TUS0                0
+#define FSM_TUS1                1
+#define FSM_WAIT                2
+#define FSM_ERR_US0_MULTI_RESP  3
+#define FSM_ERR_US1_MULTI_RESP  4
+#define FSM_ERR_US0_TIMEOUT     5
+#define FSM_ERR_US1_TIMEOUT     6
+// Index of timer
+// 0: T0
+// 1: T0
+#define TMR_IDX_0       0
+#define TMR_IDX_1       1
+// Mode of timer
+// 0: Pulse Width Test Mode
+// 1: 10 ms timer mode
+// 2: 50 ms timer mode
+#define TMR_MOD_CAP     0
+#define TMR_MOD_10MS    1
+#define TMR_MOD_50MS    2
+#define TMR_MOD_STOP    3
+// Init value of timer
+// MCU  : 6MHz 
+// T0/1 : 6/12MHz
+// 1cyc : 2us
+#define TL_INIT_10MS    0x78
+#define TH_INIT_10MS    0xEC
+#define TL_INIT_50MS    0x58
+#define TH_INIT_50MS    0x9E
+// Index of int
+// 0: INT0
+// 1: INT1
+#define INT_IDX_0       0
+#define INT_IDX_1       1
+// Mode of timer
+// 0: Enable
+// 1: Disable
+#define INT_MOD_START   0
+#define INT_MOD_STOP    1
+
+typedef struct{
+    unsigned char cnt;
+    unsigned char bak;
+} STRUCT_INT_CNT;
+
+
+//---------------------------------------------------------------------------
+// Global Variable Declearation
+//---------------------------------------------------------------------------
+//extern uint16  time_ms;
+extern u16  time_ms;
+extern u16  time_qua;
+extern u16  time_sec;
+extern u8   fsm;
+extern u8   us0_frash;
+extern u8   us1_frash;
+extern u16  us0_meas[4];
+extern u16  us1_meas[4];
+extern STRUCT_INT_CNT data st_x0;
+extern STRUCT_INT_CNT data st_x1;
+extern STRUCT_INT_CNT data st_t0;
+extern STRUCT_INT_CNT data st_t1;
+extern STRUCT_INT_CNT data *st_p;
+
+unsigned char inc_check(STRUCT_INT_CNT *p);
+void sys_init (void);
+void fsm_tus0_proc(void);
+void fsm_tus1_proc(void);
+void fsm_wait_proc(void);
+void fsm_erro_proc(void);
+
+
+
+
+
+
+#endif
